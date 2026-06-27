8c1d9fcfde5e1d6a70e1aa5339acb9d683ce0578
Mordechai <taskonway@gmail.com>
2026-06-27 15:01:24 +0300

    Initial Commit

---
+++ WayTask/WayTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayTaskApp.swift
+//  WayTask
+//
+//  Created by Mordechai Zukerman on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct WayTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
